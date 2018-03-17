5bfa32e89b6de6d51ee19ad3783d8c30e077b068
Andrew Gordon <gordon.ap@gmail.com>
2018-03-16 18:07:05 -0700

    check victory method and 1 possible combo

--- game.rb
+++ game.rb
@@ -1,33 +1,31 @@
 def setup
-  @board = ["1","2","3","4","5","6","7","8", "9"]
-  @turns = 0
+  @board = ["0","1","2","3","4","5","6","7","8"]
 end
 
 def show_board
   puts "#{@board[0]} #{@board[1]} #{@board[2]}"
   puts "#{@board[3]} #{@board[4]} #{@board[5]}"
-  puts "#{@board[6]} #{@board[7]} #{@board[8]}"
+  puts "#{@board[6]} #{@board[7]} #{@board[8]}\n\n"
 end
 
 def human_take_turn
-  puts "HUMAN TURN"
   print "Which square? "
-  @selection = gets.chomp.to_i - 1
+  @selection = gets.chomp.to_i
   check_space("human", @selection)
 end
 
 def cpu_take_turn
-  puts "CPU TURN"
-  @selection = rand(1..9)
+  @selection = rand(3..9)
   check_space("cpu", @selection)
 end
 
 def check_space(player, selection)
-  if selection == @board[selection - 1].to_i
+  if selection == @board[selection].to_i
     if player == "human"
-      @board[selection] = "X"
+      @board[selection] = "H"
     else
-      @board[selection] = "O"
+      @board[selection] = "C"
+      puts "CPU played. Your turn."
     end
   else
     if player == "human"
@@ -39,10 +37,36 @@ def check_space(player, selection)
   end
 end
 
+def check_victory
+  if @board[0] == "H" && @board[1] == "H" && @board[2] == "H"
+    puts "win!"
+  end
+end
+
 
 setup
+
 show_board
 human_take_turn
 show_board
+check_victory
+cpu_take_turn
+show_board
+
+human_take_turn
+show_board
+check_victory
+cpu_take_turn
+show_board
+
+human_take_turn
+show_board
+check_victory
+cpu_take_turn
+show_board
+
+human_take_turn
+show_board
+check_victory
 cpu_take_turn
 show_board
