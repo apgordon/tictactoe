30298704fb4b3af88f2564ae137f843ccf4ceca4
Andrew Gordon <gordon.ap@gmail.com>
2018-03-16 18:57:15 -0700

    plane is landing. saving but error with cpu making moves to return to.

--- game.rb
+++ game.rb
@@ -13,14 +13,11 @@ end
 def human_take_turn
   print "Which square? "
   @selection = gets.chomp.to_i
-  puts "@selection is #{@selection}"
   check_space("human", @selection)
 end
 
 def cpu_take_turn
-  puts "CPU playing..."
   @selection = rand(1..9)
-  puts "@selection = #{@selection}"
   check_space("cpu", @selection)
 end
 
@@ -48,7 +45,51 @@ def check_victory
   if @board[0] == "H" && @board[1] == "H" && @board[2] == "H"
     puts "You win!"
     @victory = 1
-    show_board
+  elsif @board[0] == "C" && @board[1] == "C" && @board[2] == "C"
+    puts "You lose!"
+    @victory = 1
+  elsif @board[3] == "H" && @board[4] == "H" && @board[5] == "H"
+    puts "You win!"
+    @victory = 1
+  elsif @board[3] == "C" && @board[4] == "C" && @board[5] == "C"
+    puts "You lose!"
+    @victory = 1
+  elsif @board[6] == "H" && @board[7] == "H" && @board[8] == "H"
+    puts "You win!"
+    @victory = 1
+  elsif @board[6] == "C" && @board[7] == "C" && @board[8] == "C"
+    puts "You lose!"
+    @victory = 1
+  elsif @board[0] == "H" && @board[3] == "H" && @board[6] == "H"
+    puts "You win!"
+    @victory = 1
+  elsif @board[0] == "C" && @board[3] == "C" && @board[6] == "C"
+    puts "You lose!"
+    @victory = 1
+  elsif @board[1] == "H" && @board[4] == "H" && @board[7] == "H"
+    puts "You win!"
+    @victory = 1
+  elsif @board[1] == "C" && @board[4] == "C" && @board[7] == "C"
+    puts "You lose!"
+    @victory = 1
+  elsif @board[2] == "H" && @board[5] == "H" && @board[8] == "H"
+    puts "You win!"
+    @victory = 1
+  elsif @board[2] == "C" && @board[5] == "C" && @board[8] == "C"
+    puts "You lose!"
+    @victory = 1
+  elsif @board[0] == "H" && @board[4] == "H" && @board[8] == "H"
+    puts "You win!"
+    @victory = 1
+  elsif @board[0] == "C" && @board[4] == "C" && @board[8] == "C"
+    puts "You lose!"
+    @victory = 1
+  elsif @board[2] == "H" && @board[4] == "H" && @board[6] == "H"
+    puts "You win!"
+    @victory = 1
+  elsif @board[2] == "C" && @board[4] == "C" && @board[6] == "C"
+    puts "You lose!"
+    @victory = 1
   end
 end
 
