9c1d9c35878d5497dddf5a0b8935b67f4bad0a99
Andrew Gordon <gordon.ap@gmail.com>
2018-03-19 22:32:17 -0400

    gameplay works, checking for victory works, but computer dumb.

--- game.rb
+++ game.rb
@@ -1,13 +1,13 @@
 def setup
-  @board = ["1","2","3","4","5","6","7","8","9"]
-  @whose_turn = "H"
+  @board = ["0", "1","2","3","4","5","6","7","8","9"]
+  @whose_turn = "C"
   @victory = 0
 end
 
 def show_board
-  puts "#{@board[0]} #{@board[1]} #{@board[2]}"
-  puts "#{@board[3]} #{@board[4]} #{@board[5]}"
-  puts "#{@board[6]} #{@board[7]} #{@board[8]}\n\n"
+  puts "#{@board[1]} #{@board[2]} #{@board[3]}"
+  puts "#{@board[4]} #{@board[5]} #{@board[6]}"
+  puts "#{@board[7]} #{@board[8]} #{@board[9]}\n\n"
 end
 
 def human_take_turn
@@ -22,13 +22,13 @@ def cpu_take_turn
 end
 
 def check_space(player, selection)
-  if selection == @board[selection - 1].to_i
+  if selection == @board[selection].to_i
     if player == "human"
-      @board[selection -1] = "H"
+      @board[selection] = "H"
       @whose_turn = "C"
-    else
+    elsif player == "cpu"
       @board[selection] = "C"
-      puts "CPU played. Your turn."
+      puts "CPU played #{selection}. Your turn."
       @whose_turn = "H"
     end
   else
@@ -42,28 +42,22 @@ def check_space(player, selection)
 end
 
 def check_victory
-  if @board[0] == "H" && @board[1] == "H" && @board[2] == "H"
+  if @board[1] == "H" && @board[2] == "H" && @board[3] == "H"
     puts "You win!"
     @victory = 1
-  elsif @board[0] == "C" && @board[1] == "C" && @board[2] == "C"
+  elsif @board[1] == "C" && @board[2] == "C" && @board[3] == "C"
     puts "You lose!"
     @victory = 1
-  elsif @board[3] == "H" && @board[4] == "H" && @board[5] == "H"
+  elsif @board[4] == "H" && @board[5] == "H" && @board[6] == "H"
     puts "You win!"
     @victory = 1
-  elsif @board[3] == "C" && @board[4] == "C" && @board[5] == "C"
+  elsif @board[4] == "C" && @board[5] == "C" && @board[6] == "C"
     puts "You lose!"
     @victory = 1
-  elsif @board[6] == "H" && @board[7] == "H" && @board[8] == "H"
+  elsif @board[7] == "H" && @board[8] == "H" && @board[9] == "H"
     puts "You win!"
     @victory = 1
-  elsif @board[6] == "C" && @board[7] == "C" && @board[8] == "C"
-    puts "You lose!"
-    @victory = 1
-  elsif @board[0] == "H" && @board[3] == "H" && @board[6] == "H"
-    puts "You win!"
-    @victory = 1
-  elsif @board[0] == "C" && @board[3] == "C" && @board[6] == "C"
+  elsif @board[7] == "C" && @board[8] == "C" && @board[9] == "C"
     puts "You lose!"
     @victory = 1
   elsif @board[1] == "H" && @board[4] == "H" && @board[7] == "H"
@@ -78,16 +72,22 @@ def check_victory
   elsif @board[2] == "C" && @board[5] == "C" && @board[8] == "C"
     puts "You lose!"
     @victory = 1
-  elsif @board[0] == "H" && @board[4] == "H" && @board[8] == "H"
+  elsif @board[3] == "H" && @board[6] == "H" && @board[9] == "H"
     puts "You win!"
     @victory = 1
-  elsif @board[0] == "C" && @board[4] == "C" && @board[8] == "C"
+  elsif @board[3] == "C" && @board[6] == "C" && @board[9] == "C"
     puts "You lose!"
     @victory = 1
-  elsif @board[2] == "H" && @board[4] == "H" && @board[6] == "H"
+  elsif @board[1] == "H" && @board[5] == "H" && @board[9] == "H"
     puts "You win!"
     @victory = 1
-  elsif @board[2] == "C" && @board[4] == "C" && @board[6] == "C"
+  elsif @board[1] == "C" && @board[5] == "C" && @board[9] == "C"
+    puts "You lose!"
+    @victory = 1
+  elsif @board[3] == "H" && @board[5] == "H" && @board[7] == "H"
+    puts "You win!"
+    @victory = 1
+  elsif @board[3] == "C" && @board[5] == "C" && @board[7] == "C"
     puts "You lose!"
     @victory = 1
   end
